373c500ae79e88043aa4fd4369595abf4f2d5ae6
Guilherme <>
2020-09-08 11:17:02 -0300

    Correção j2ee padrão.

---
+++ src/main/webapp/assets/js/user.js
@@ -0,0 +1,62 @@
+/*
+ * Checks whether the password and password-confirm fields match
+ */
+$(document).on('focusout', '.password-input,.password-confirm', function(e) {
+    var $form = $(this).closest("form");
+    var $password = $form.find(".password-input");
+    var $passwordConfirm = $form.find(".password-confirm");
+
+    if ($password.val().trim() == '') {
+        return false;
+    }
+
+    if ($password.val() !== $passwordConfirm.val()) {
+        $passwordConfirm.closest('.form-group').addClass('has-error');
+        $password.closest('.form-group').addClass('has-error');
+        $passwordConfirm.next('p.help-block').html('<strong>Erro</strong>: as senhas não coincidem!');
+        $form.find("button,input[type='submit']").prop('disabled', true);
+    } else {
+        $passwordConfirm.closest('.form-group').removeClass('has-error').addClass('has-success');
+        $password.closest('.form-group').removeClass('has-error').addClass('has-success');
+        $passwordConfirm.next('p.help-block').html('');
+        $form.find("button,input[type='submit']").prop('disabled', false);
+    }
+});
+
+function deleteUser(e) {
+    e.preventDefault();
+    $('.link_confirmacao_excluir_usuario').attr('href', $(this).data('href'));
+    $('.modal_excluir_usuario').modal();
+}
+
+function deleteUsers(e) {
+    e.preventDefault();
+    $('.form_excluir_usuarios').submit();
+}
+
+function readUser(e) {
+    e.preventDefault();
+    $.get($(this).data('href'), function (data) {
+        var usuario = JSON.parse(data);
+        var $modal = $('.modal-visualizar-usuario');
+
+        $modal.find('.p_id').html('<strong>ID: </strong>' + usuario.id);
+        $modal.find(".p_login").html('<strong>Login: </strong>' + usuario.login);
+        $modal.find('.p_nome').html('<strong>Nome: </strong>' + usuario.nome);
+        $modal.find('.p_nascimento').html('<strong>Data de nascimento: </strong>' + usuario.nascimento);
+        if (usuario.avatar) {
+            $modal.find('.usuario-img').prop('src', $.url("//img/" + usuario.avatar));
+        }
+
+        $modal.modal();
+    });
+}
+
+$(document).ready(function () {
+    $(document).on('click', '.link_excluir_usuario', deleteUser);
+    $(document).on('click', '.button_confirmacao_excluir_usuarios', deleteUsers);
+    $(document).on('click', '.link_visualizar_usuario', readUser);    
+    $("*[data-toggle='tooltip']").tooltip({
+        'container': 'body'
+    });
+});
