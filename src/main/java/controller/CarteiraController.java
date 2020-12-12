6ba236659214e9e9cc2bbbd0dfe52f002145bccd
mathe <mathmatiazzo@gmail.com>
2020-12-12 14:23:38 -0300

    Carteira controller update

---
+++ src/main/java/controller/CarteiraController.java
@@ -0,0 +1,137 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import dao.DAO;
+import dao.DAOFactory;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import model.Carteira;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import static sun.font.CreatedFontTracker.MAX_FILE_SIZE;
+
+/**
+ *
+ * @author mathe
+ */
+@WebServlet(
+        name = "CarteiraController",
+        urlPatterns = {
+            "/carteira/update",
+        })
+public class CarteiraController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+//        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        DAO<Carteira, String> dao;
+        Carteira carteira = new Carteira();
+        HttpSession session = request.getSession();
+
+        String servletPath = request.getServletPath();
+
+        switch (request.getServletPath()) {
+            case "/carteira/update": {
+                // Se fosse um form simples, usaria request.getParameter()
+                // String login = request.getParameter("login");
+
+                // Manipulação de form com enctype="multipart/form-data"
+                // Create a factory for disk-based file items
+                DiskFileItemFactory factory = new DiskFileItemFactory();
+                // Set the directory used to temporarily store files that are larger than the configured size threshold
+                factory.setRepository(new File("/tmp"));
+                // Create a new file upload handler
+                ServletFileUpload upload = new ServletFileUpload(factory);
+
+                try ( DAOFactory daoFactory = DAOFactory.getInstance()) {
+                    // Parse the request
+                    List<FileItem> items = upload.parseRequest(request);
+
+                    // Process the uploaded items
+                    Iterator<FileItem> iter = items.iterator();
+                    while (iter.hasNext()) {
+                        FileItem item = iter.next();
+
+                        // Process a regular form field
+                        if (item.isFormField()) {
+                            String fieldName = item.getFieldName();
+                            String fieldValue = item.getString();
+
+                            switch (fieldName) {
+                                case "cpf":
+                                    carteira.setCpf(fieldValue);
+                                    break;
+                                case "saldo":
+                                    carteira.setSaldo(Double.parseDouble(fieldValue));
+                                    break;
+                            }
+                        } 
+                    }
+
+                    dao = daoFactory.getCarteiraDAO();
+
+                    if (servletPath.equals("/car/create")) {
+                        dao.create(carteira);
+                    } else {
+                        servletPath += "?cpf=&saldo=?" + String.valueOf(carteira.getCpf());
+                        dao.update(carteira);
+                    }
+
+                    response.sendRedirect(request.getContextPath() + "/fluxo/profile");
+                    
+                } catch (FileUploadException ex) {
+                    Logger.getLogger(CarController.class.getName()).log(Level.SEVERE, "Controller", ex);
+                    session.setAttribute("error", "Erro ao fazer upload do arquivo.");
+                    response.sendRedirect(request.getContextPath() + servletPath);
+                } catch (ClassNotFoundException | IOException | SQLException ex) {
+                    Logger.getLogger(CarController.class.getName()).log(Level.SEVERE, "Controller", ex);
+                    session.setAttribute("error", ex.getMessage());
+                    response.sendRedirect(request.getContextPath() + servletPath);
+                } catch (Exception ex) {
+                    Logger.getLogger(CarController.class.getName()).log(Level.SEVERE, "Controller", ex);
+                    session.setAttribute("error", "Erro ao gravar arquivo no servidor.");
+                    response.sendRedirect(request.getContextPath() + servletPath);
+                }
+                break;
+            }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+
+}}
